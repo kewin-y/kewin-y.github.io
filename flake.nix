f8a129e8d0257c66eaa5da45b43a14392acd7860
Kevin <jekevin964@gmail.com>
2026-03-04 23:08:12 -0500

    stuff

--- flake.nix
+++ flake.nix
@@ -1,46 +1,46 @@
 {
-  description = "devshell for my personal website";
+    description = "devshell for my personal website";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
 
-  outputs = {nixpkgs, ...}: let
-    forAllSystems = nixpkgs.lib.genAttrs [
-      "aarch64-linux"
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-  in {
-    formatter = forAllSystems (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        pkgs.alejandra
-    );
-    devShells = forAllSystems (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = pkgs.mkShell {
-          packages = builtins.attrValues {
-            inherit
-              (pkgs)
-              nodejs
-              pnpm
-              prettierd
-              typescript-language-server
-              tailwindcss-language-server
-              ;
-          };
+    outputs = {nixpkgs, ...}: let
+        forAllSystems = nixpkgs.lib.genAttrs [
+            "aarch64-linux"
+            "x86_64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+        ];
+    in {
+        formatter = forAllSystems (
+            system: let
+                pkgs = nixpkgs.legacyPackages.${system};
+            in
+                pkgs.alejandra
+        );
+        devShells = forAllSystems (
+            system: let
+                pkgs = nixpkgs.legacyPackages.${system};
+            in {
+                default = pkgs.mkShell {
+                    packages = builtins.attrValues {
+                        inherit
+                            (pkgs)
+                            nodejs
+                            pnpm
+                            prettierd
+                            typescript-language-server
+                            tailwindcss-language-server
+                            astro-language-server
+                            ;
+                    };
 
-          shellHook = ''
-            export SHELL='${pkgs.mksh}/bin/mksh'
-          '';
-        };
-      }
-    );
-  };
+                    shellHook = ''
+                        export SHELL='${pkgs.mksh}/bin/mksh'
+                    '';
+                };
+            }
+        );
+    };
 }
-
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,12 @@ import { z, defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blogs = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blogs" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-  }),
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blogs" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+    }),
 });
 
-// Expose your defined collection to Astro
-// with the `collections` export
 export const collections = { blogs };
