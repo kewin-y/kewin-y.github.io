8221ca6b5c1532fbb827e8917bcd8f0f514d5b95
Kevin <jekevin964@gmail.com>
2026-03-06 02:56:35 -0500

    update shiki theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
-  site: "https://kewin-y.github.io/",
+    site: "https://kewin-y.github.io/",
+    integrations: [mdx()],
+    markdown: {
+        syntaxHighlight: "shiki",
+        shikiConfig: {
+            theme: "material-theme-lighter",
+            wrap: true,
+        },
+    },
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
+
